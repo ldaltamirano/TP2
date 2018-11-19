8092ef0cdea5e60c19f5f034b39ac5cdafddaebe
ldaltamirano <lalt123@gmail.com>
2018-11-19 17:23:08 -0300

    Se agrega carpeta api vendor y demas

---
+++ api/app/RedSocial/Models/Perfil.php
@@ -0,0 +1,42 @@
+<?php
+namespace TP2\Models;
+
+use Exception;
+use TP2\DB\DBConection;
+
+class Perfil {
+    protected $id;
+    protected $perfil;
+
+    public Perfil() {}
+    public Perfil($idPerfil) {
+        $db = DBConnection::getConnection();
+		$query = "SELECT ID_PERFIL, PERFIL FROM perfil
+				WHERE ID = ?";
+		$stmt = $db->prepare($query);
+		$stmt->execute([$idPerfil]);
+		if($fila = $stmt->fetch()) {
+			$this->cargarDatosDeArray($fila);
+        }
+        //TODO: Probar retornar nulo
+    }
+
+        	/**
+	 * Carga los datos de la $fila en el objeto.
+	 *
+	 * @param array $fila
+	 */
+	public function cargarDatosDeArray($fila)
+	{
+		$this->idPerfil	    = $fila['ID'];
+        $this->perfil       = $fila['PERFIL'];
+    }
+
+    /**
+    * Devuelve el dato $perfil del objeto usuario
+    */
+    public function getPerfil() {
+        return $this->perfil;
+    }
+
+}
