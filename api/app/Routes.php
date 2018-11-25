3f98e77a1178d99a947a2b6127b3a20189bbac16
ldaltamirano <lalt123@gmail.com>
2018-11-25 02:32:46 -0300

    Route.php - ruteo

---
+++ api/app/Routes.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Este archivo va a contener TODAS las rutas de
+ * nuestra aplicación.
+ *
+ * Para esto, vamos a crear una clase Route cuya
+ * función sea la de registrar y administrar las rutas.
+ */
+use Redsocial\Core\Route;
+
+Route::add('POST', '/login', 'AuthController@login');
+Route::add('GET', '/publicaciones', 'PublicacionesController@todos');
+Route::add('GET', '/publicaciones/{id}', 'PublicacionesController@detalle');
+
+Route::add('POST', '/publicaciones', 'PublicacionesController@crear');
+Route::add('PUT', '/publicaciones/{id}', 'publicacionesController@editar');
+Route::add('DELETE', '/publicaciones/{id}', 'publicacionesController@eliminar');
